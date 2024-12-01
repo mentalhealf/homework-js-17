d15ebd39689429a06d7dcb9948c99002e8f6d44e
mentalhealf <147326241+mentalhealf@users.noreply.github.com>
2024-12-01 19:17:51 +0000

    df

---
+++ index.js
@@ -0,0 +1,30 @@
+// 1. Створити розмітку з кнопкою та текстовим полем. За допомогою 
+// JavaScript отримати доступ до кнопки та текстового поля за їх
+//  ідентифікаторами та змінити текст на кнопці на значення текстового поля.
+
+// const button = document.getElementById('changeButton');
+// const input = document.getElementById('textInput');
+// button.textContent = input.value;
+
+// 2.Створити розмітку з заголовком та зображенням.
+//  За допомогою JavaScript отримати доступ до зображення та змінити значення атрибута "src" на шлях до іншого зображення.
+
+// const image = document.getElementById('image');
+// image.src = 'hudwink.jpeg';
+
+// 3.Створити розмітку з посиланням та зображенням.
+//  За допомогою JavaScript отримати доступ до посилання та змінити значення атрибута "href" на нову URL-адресу.
+//  Також отримати доступ до зображення та додати новий атрибут "alt" з описом зображення.
+
+// const link = document.getElementById('link');
+// const linkedImage = document.getElementById('linkedImage');
+// link.href = 'https://Hudwink-Fan.com';
+// linkedImage.alt = 'Hudwink';
+
+
+// 4.Створити розмітку зі списком елементів.
+//  За допомогою JavaScript отримати доступ до першого елемента списку та змінити його вміст на новий текст.
+
+// const list = document.getElementById('list');
+// const firstListItem = list.querySelector('li');
+// firstListItem.textContent = 'Елемент 1.1';
